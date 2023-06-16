206565873b36d53893ed82af945bb72c176968a9
System Administrator <root@MacBook-Air-Admin.local>
2023-06-16 05:50:27 +0300

    dev: db connect ip

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 const { PORT = 3000 } = process.env;
-const webaip = '158.160.58.14'
+const webip = '158.160.58.14'
 const JWT_SECRET = '5fb40adfd86a6be9c48b0b04335b688f8a1ab09d525f7c6799c51e6572080291';
 const DB_ADDRESS = `mongodb://${webip}:27017/mestodb`; const SALT_ROUNDS = 10;
 module.exports = {
