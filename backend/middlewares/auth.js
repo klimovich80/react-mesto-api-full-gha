5f88fcaf0ab143891b20144309b5c5a0e1dd95d7
System Administrator <root@MacBook-Air-Admin.local>
2023-06-21 12:17:26 +0300

    dev: env tests jwt_secret 3

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
-
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
@@ -19,7 +17,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : '');
   } catch (err) {
     return errorHandler(authError, next);
   }
